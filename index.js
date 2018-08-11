ebc44df2e4de86b0cfb4fbe71f857bdde3a0bf27
yinm <yinm.cminor9@gmail.com>
2018-08-11 10:47:21 +0900

    Extract method

--- index.js
+++ index.js
@@ -10,62 +10,68 @@ const API_URL = `https://api.trello.com/1/lists/${LIST_ID}/cards?key=${API_KEY}&
 request.get(API_URL)
   .then(response => {
     const res = JSON.parse(response)
-    const textThirtyMinutes = '30minutes';
-    const textOneHour = '1hour'
-    const textTwoHours = '2hours'
-    const textThreeHours = '3hours'
-
-    let total = 0
-    let thirtyMinutes = 0
-    let oneHour = 0
-    let twoHours = 0
-    let threeHours = 0
-    let noLabel = 0
-    let whatsHappen = 0
-
-    for (let card of res) {
-      if (card.labels[0] === undefined) {
-        noLabel++
-        continue
-      }
-
-      switch (card.labels[0].name) {
-        case textThirtyMinutes:
-          thirtyMinutes++
-          total += 0.5
-          break
-        case textOneHour:
-          oneHour++
-          total += 1
-          break
-        case textTwoHours:
-          twoHours++
-          total += 2
-          break
-        case textThreeHours:
-          threeHours++
-          total += 3
-          break
-        default:
-          whatsHappen++
-      }
-    }
-
-    const object = {
-      'total': total,
-      [textThirtyMinutes]: thirtyMinutes,
-      [textOneHour]: oneHour,
-      [textTwoHours]: twoHours,
-      [textThreeHours]: threeHours,
-      'no label': noLabel,
-      "what's happen!!!": whatsHappen,
-    }
-
-    render(object)
+    render(aggregateTime(res))
   })
   .catch(error => {
     console.log(error)
   })
+
+/**
+ * @param {Object} response
+ * @returns {Object}
+ */
+function aggregateTime(response) {
+  const textThirtyMinutes = '30minutes';
+  const textOneHour = '1hour'
+  const textTwoHours = '2hours'
+  const textThreeHours = '3hours'
+
+  let total = 0
+  let thirtyMinutes = 0
+  let oneHour = 0
+  let twoHours = 0
+  let threeHours = 0
+  let noLabel = 0
+  let whatsHappen = 0
+
+  for (let card of response) {
+    if (card.labels[0] === undefined) {
+      noLabel++
+      continue
+    }
+
+    switch (card.labels[0].name) {
+      case textThirtyMinutes:
+        thirtyMinutes++
+        total += 0.5
+        break
+      case textOneHour:
+        oneHour++
+        total += 1
+        break
+      case textTwoHours:
+        twoHours++
+        total += 2
+        break
+      case textThreeHours:
+        threeHours++
+        total += 3
+        break
+      default:
+        whatsHappen++
+    }
+  }
+
+  return {
+    'total': total,
+    [textThirtyMinutes]: thirtyMinutes,
+    [textOneHour]: oneHour,
+    [textTwoHours]: twoHours,
+    [textThreeHours]: threeHours,
+    'no label': noLabel,
+    "what's happen!!!": whatsHappen,
+  }
+}
 
 function render(object) {
   for (let key of Object.keys(object)) {
